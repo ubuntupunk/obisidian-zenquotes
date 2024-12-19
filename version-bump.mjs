464e198d6a365d388d8cd634f176e2d6fb1f90ca
ubuntupunk@gmail.com <ubuntupunk@gmail.com>
2024-12-19 19:47:29 +0200

    chore: bump version to 1.0.6

--- version-bump.mjs
+++ version-bump.mjs
@@ -1,6 +1,14 @@
 import { readFileSync, writeFileSync } from "fs";
 
-const targetVersion = process.env.npm_package_version;
+// Read current version from package.json
+const packageJson = JSON.parse(readFileSync("package.json", "utf8"));
+let [major, minor, patch] = packageJson.version.split('.').map(Number);
+patch += 1; // Increment patch version
+const targetVersion = `${major}.${minor}.${patch}`;
+
+// Update package.json
+packageJson.version = targetVersion;
+writeFileSync("package.json", JSON.stringify(packageJson, null, "\t"));
 
 // read minAppVersion from manifest.json and bump version to target version
 let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
@@ -12,3 +20,5 @@ writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
 let versions = JSON.parse(readFileSync("versions.json", "utf8"));
 versions[targetVersion] = minAppVersion;
 writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
+
+console.log(`Version bumped to ${targetVersion}`);
