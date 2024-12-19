7a091b27657c7081b7bb78ca0e179c6c8f53ee96
ubuntupunk@gmail.com <ubuntupunk@gmail.com>
2024-12-19 11:28:08 +0200

    Update quote mode settings and API integration

--- main.ts
+++ main.ts
@@ -4,25 +4,31 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 
 interface MyPluginSettings {
 	mySetting: string;
+	showRibbonIcon: boolean;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	mySetting: 'random',
+	showRibbonIcon: true
 }
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
+	ribbonIconEl: HTMLElement | null = null;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'XenQuotes Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		if (this.settings.showRibbonIcon) {
+			this.ribbonIconEl = this.addRibbonIcon('dice', 'XenQuotes Plugin', (evt: MouseEvent) => {
+				// Called when the user clicks the icon.
+				new Notice('This is a notice!');
+			});
+			// Perform additional things with the ribbon
+			if (this.ribbonIconEl) {
+				this.ribbonIconEl.addClass('my-plugin-ribbon-class');
+			}
+		}
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
@@ -52,7 +58,8 @@ export default class MyPlugin extends Plugin {
 			name: 'Fetch Quote of the Day',
 			editorCallback: async (editor: Editor, view: MarkdownView) => {
 				try {
-					const response = await requestUrl('https://zenquotes.io/api/random');
+					const mode = this.settings.mySetting;
+					const response = await requestUrl(`https://zenquotes.io/api/${mode}`);
 					if (response.status === 200) {
 						const quoteData = response.json;
 						if (quoteData && quoteData.length > 0) {
@@ -70,16 +77,6 @@ export default class MyPlugin extends Plugin {
 					console.error("Error fetching quote:", error);
 					new Notice("An error occurred while fetching the quote. Check console for details.");
 				}
-			}
-		});
-
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
 			}
 		});
 
@@ -159,14 +156,37 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Choose quotation mode')
+			.setDesc('Choose from [quotes, today, author, random]')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
+				.setPlaceholder('Enter your mode here')
 				.setValue(this.plugin.settings.mySetting)
 				.onChange(async (value) => {
 					this.plugin.settings.mySetting = value;
 					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Show Ribbon Icon')
+			.setDesc('Toggle the visibility of the ribbon icon')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.showRibbonIcon)
+				.onChange(async (value) => {
+					this.plugin.settings.showRibbonIcon = value;
+					await this.plugin.saveSettings();
+					if (value) {
+						this.plugin.ribbonIconEl = this.plugin.addRibbonIcon('dice', 'XenQuotes Plugin', (evt: MouseEvent) => {
+							new Notice('This is a notice!');
+						});
+						if (this.plugin.ribbonIconEl) {
+							this.plugin.ribbonIconEl.addClass('my-plugin-ribbon-class');
+						}
+					} else {
+						if (this.plugin.ribbonIconEl) {
+							this.plugin.ribbonIconEl.remove();
+							this.plugin.ribbonIconEl = null;
+						}
+					}
 				}));
 	}
 }
